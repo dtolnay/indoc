e550e387465efc6956b510a951d99c1d80675cc9
David Tolnay <dtolnay@gmail.com>
2016-03-19 12:29:23 -0700

    Use to_owned instead of to_string in test runner

--- tests/tests.rs
+++ tests/tests.rs
@@ -6,7 +6,7 @@ fn run_mode(mode: &'static str) {
     let mut config = compiletest::default_config();
     config.mode = mode.parse().unwrap();
     config.src_base = PathBuf::from(format!("tests/{}", mode));
-    config.target_rustcflags = Some("--extern indoc=target/debug/libindoc.so".to_string());
+    config.target_rustcflags = Some("--extern indoc=target/debug/libindoc.so".to_owned());
     compiletest::run_tests(&config);
 }
 

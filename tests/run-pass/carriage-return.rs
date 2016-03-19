74e4ddde696d12b4820a70fd4423cb0e89de2dc7
David Tolnay <dtolnay@gmail.com>
2016-03-19 12:55:54 -0700

    Add test case containing carriage returns

---
+++ tests/run-pass/carriage-return.rs
@@ -0,0 +1,13 @@
+#![feature(plugin)]
+#![plugin(indoc)]
+
+fn main() {
+    // Every line in the string ends with \r\n
+    let indoc = indoc!("
+        a
+
+            \\b
+        c");
+    let expected = "a\n\n    \\b\nc";
+    assert_eq!(indoc, expected);
+}

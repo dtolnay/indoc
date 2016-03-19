4cdbfd780465be20457d94a11f9be6acaf1f258b
David Tolnay <dtolnay@gmail.com>
2016-03-19 12:29:05 -0700

    Remove unnecessary error check in test runner

--- tests/tests.rs
+++ tests/tests.rs
@@ -4,12 +4,9 @@ use std::path::PathBuf;
 
 fn run_mode(mode: &'static str) {
     let mut config = compiletest::default_config();
-    let cfg_mode = mode.parse().ok().expect("Invalid mode");
-
-    config.mode = cfg_mode;
+    config.mode = mode.parse().unwrap();
     config.src_base = PathBuf::from(format!("tests/{}", mode));
     config.target_rustcflags = Some("--extern indoc=target/debug/libindoc.so".to_string());
-
     compiletest::run_tests(&config);
 }
 

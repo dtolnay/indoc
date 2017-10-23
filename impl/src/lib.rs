54e1566cf784e29ec4553fc4cefac2e9375d1264
David Tolnay <dtolnay@gmail.com>
2017-10-23 09:23:08 -0700

    Release 0.2.2

--- impl/src/lib.rs
+++ impl/src/lib.rs
@@ -6,7 +6,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![doc(html_root_url = "https://docs.rs/indoc-impl/0.2.1")]
+#![doc(html_root_url = "https://docs.rs/indoc-impl/0.2.2")]
 
 #![cfg_attr(feature = "unstable", feature(proc_macro))]
 
--- src/lib.rs
+++ src/lib.rs
@@ -102,7 +102,7 @@
 //!    ")            /            ")            /
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/indoc/0.2.1")]
+#![doc(html_root_url = "https://docs.rs/indoc/0.2.2")]
 
 #![cfg_attr(feature = "unstable", feature(decl_macro, proc_macro, use_extern_macros))]
 

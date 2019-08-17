59ce37953a180eae632500cfe92a06b2de5670fd
David Tolnay <dtolnay@gmail.com>
2019-08-17 13:03:53 -0700

    Remove unneeded license boilerplates

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-// Copyright 2016 Indoc Developers
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 //! This crate provides a procedural macro for indented string literals. The
 //! `indoc!()` macro takes a multiline string literal and un-indents it so the
 //! leftmost non-space character is in the first column.

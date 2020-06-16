b940aa43b5f17b332080d1d50c4450eee71b753b
David Tolnay <dtolnay@gmail.com>
2020-06-16 12:06:06 -0700

    Release unindent 0.1.6

--- unindent/src/lib.rs
+++ unindent/src/lib.rs
@@ -45,7 +45,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/unindent/0.1.5")]
+#![doc(html_root_url = "https://docs.rs/unindent/0.1.6")]
 #![cfg_attr(feature = "cargo-clippy", allow(type_complexity))]
 
 use std::iter::Peekable;

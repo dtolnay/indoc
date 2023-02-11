a4179b5ee3e4dfee43deb404cacb7609f869990c
David Tolnay <dtolnay@gmail.com>
2023-02-10 21:01:15 -0800

    Release unindent 0.2.1

--- unindent/src/lib.rs
+++ unindent/src/lib.rs
@@ -45,7 +45,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/unindent/0.2.0")]
+#![doc(html_root_url = "https://docs.rs/unindent/0.2.1")]
 #![allow(
     clippy::missing_panics_doc,
     clippy::module_name_repetitions,
